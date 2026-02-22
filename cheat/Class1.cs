a38e907274adcd44d350be64ecca077334cd3c4a
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-22 20:53:16 +0100

    create gitignore & .net project

---
+++ cheat/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cheat
+{
+    public class Class1
+    {
+    }
+}
